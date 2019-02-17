c0fa7a2cd59927536af36420a1e32423c738a5b0
rykn0wxx <raurici666@gmail.com>
2019-02-17 13:50:45 -0500

    done session view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
+  layout :set_layout
+
   private
     def after_sign_out_path_for resource_or_scope
       new_sys_user_session_path
@@ -14,5 +16,14 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
     end
+
+    def set_layout
+      if devise_controller?
+        'authentication'
+      else
+        'application'
+      end
+    end
+    
 
 end
